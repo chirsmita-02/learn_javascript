167143b9b190daf4992f50a500e69d21eb47f30d
Chirsmita Thakur <kchirsmita@gmail.com>
2025-09-06 18:05:01 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=144553
+let accountEmail="chir@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;          //undefined output
+accountEmail="g@hc.com"
+accountPassword="1222121"
+accountCity="Bengaluru"
+
+
+//accountId=2  //not allowed
+console.log(accountId);
+console.table([accountEmail,accountCity,accountPassword,accountState])
+/*dont use var due to issue in scope of blocka nd funtion
+*/ 
