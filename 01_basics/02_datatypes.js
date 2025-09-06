ff39ba02e5782684c00eb588b4f6719e3565de60
Chirsmita Thakur <kchirsmita@gmail.com>
2025-09-06 18:52:36 +0530

    datatypes and conversionoperations

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";//treat all js code as newer version
+
+//alert("hello") //we are using nod ejs and not browser
+//code readability is important
+let name="me"
+let age=18
+let isloggedin =false
+//number =>2^53
+//bigint
+//string''""
+//boolean
+//null--standalone value
+//undefined---when value is not present
+//symbol=>unique
+
+
+//object
+
+console.log(typeof age);//number
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score ="33abs"//null=0,undefined=NaN,true=1
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);//nan
+
+let isLoggedIn=23
+let booleanISLoggedIn=Boolean(isLoggedIn)
+console.log(booleanISLoggedIn);
+//1=t
+//0=f
+//""=F,"HI"=T
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(typeof stringNumber);
